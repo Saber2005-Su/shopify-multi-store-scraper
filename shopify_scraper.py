3b887ff2281cf7577c9cddde272958b1e5880962
Saber2005-Su <saberkhan9593578755@gmail.com>
2026-07-03 01:44:09 +0530

    Integrate Gymshark and enhance error handling
    Added Gymshark to the list of stores and improved error handling for JSON decoding.

--- shopify_scraper.py
+++ shopify_scraper.py
@@ -5,44 +5,42 @@ import requests
 stores = [
     "https://www.glossier.com/products.json",
     "https://www.allbirds.com/products.json",
-    
+    "https://www.gymshark.com/products.json"
 ]
 
 for store_url in stores:
     all_products = []
     page = 1
-    
+
     while True:
         url = f'{store_url}?page={page}'
         try:
             response = requests.get(url)
             response.raise_for_status()
             data = json.loads(response.text)
-        except requests.exceptions.RequestException as e:
+        except (requests.exceptions.RequestException, json.JSONDecodeError) as e:
             print(f'Failed to fetch from:{store_url}: {e}')
             break
-        
+
         products = data.get('products', [])
-        
+
         if not products:
             break
-        
+
         for product in products:
             product_title = product.get('title', '')
             product_type = product.get('product_type', '')
-            
-            
+
             for variant in product.get('variants', []):
                 variant_title = variant.get('title', '')
                 price = variant.get('price', 0)
-                
 
                 images = product.get('images', [])
                 if images:
                     image_url = images[0].get('src', '')
                 else:
                     image_url = 'No Image'
-                
+
                 all_products.append({
                     'product_title': product_title,
                     'product_type': product_type,
@@ -50,9 +48,9 @@ for store_url in stores:
                     'price': price,
                     'image_url': image_url
                 })
-        
+
         page += 1
-    
+
     if all_products:
         store_name = store_url.split('//')[1].replace('www.', '').split('.')[0]
         file_name = f'{store_name}_product.csv'
